bf8a61fa79b0cb0a8924530d44e10902b120fde5
v.bokor <v.bokor@tinkoff.ru>
2023-12-06 18:52:56 +0700

    add-day5

---
+++ src/day05/task2.clj
@@ -0,0 +1,74 @@
+(ns day05.task2
+  (:require [clojure.string :as str])
+  )
+
+(defn get-rest-lines [lines]
+  (drop-while #(not (re-find #" map:" %)) (rest lines))
+  )
+
+(defn get-map [lines]
+  (->> (rest lines)
+       (take-while #(not (re-find #" map:" %)))
+       (drop-last)
+       (mapv #(mapv parse-long (str/split % #"\s+")))))
+   
+(defn get-value [value, current_map]
+  (let [[dest source length] current_map
+        diff (- value source)]
+    (if (and (>= value source) (>= length diff))
+      (+ dest diff))))
+
+(defn get-next-value[value, maps]
+  (let [result (remove nil? (map (partial get-value value) maps))]
+    (if (empty? result) value (first result))
+    )
+)
+
+(defn go-through-maps[value, maps]
+  (let [next-value (get-next-value value (first maps))
+        ]
+    (if (= (count maps) 1)
+      next-value
+      (go-through-maps next-value (rest maps)) 
+    )
+  )
+)
+
+(defn find-min-for-pair [pair, maps]
+  (let [[start, length] pair]
+    (->>
+     (range start (+ start length))
+     (map #(go-through-maps % maps))
+     (reduce min)
+  ))
+)
+
+(with-open [rdr (clojure.java.io/reader "assets/day05/test_input.txt")]
+  (let [lines (line-seq rdr)
+        seed-line (first lines)
+        seed-to-soil-lines (get-rest-lines lines)
+        soil-to-fertilizer-lines (get-rest-lines seed-to-soil-lines)
+        fertilizer-to-water-lines (get-rest-lines soil-to-fertilizer-lines)
+        water-to-light-lines (get-rest-lines fertilizer-to-water-lines)
+        light-to-temperature-lines (get-rest-lines water-to-light-lines)
+        temperature-to-humidity-lines (get-rest-lines light-to-temperature-lines)
+        humidity-to-location-lines (get-rest-lines temperature-to-humidity-lines)
+
+        seeds (mapv parse-long (rest (str/split seed-line #"\s+")))
+        seed-to-soils (get-map seed-to-soil-lines)
+        soil-to-fertilizers (get-map soil-to-fertilizer-lines)
+        fertilizer-to-waters (get-map fertilizer-to-water-lines)
+        water-to-lights (get-map water-to-light-lines)
+        light-to-temperatures (get-map light-to-temperature-lines)
+        temperature-to-humidities (get-map temperature-to-humidity-lines)
+        humidity-to-locations (->>
+                               (rest humidity-to-location-lines)
+                               (mapv #(mapv parse-long (str/split % #"\s+"))))
+
+        maps [seed-to-soils soil-to-fertilizers fertilizer-to-waters water-to-lights light-to-temperatures temperature-to-humidities humidity-to-locations]]
+
+    ;; "Not working on big input :sadcat"
+    (->>
+     (partition 2 seeds)
+     (map #(find-min-for-pair % maps))
+     (apply min))))
